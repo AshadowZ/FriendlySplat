e69d16c860078fcd713337eb8d36ed465770ce34
x299 <1678807085@qq.com>
2026-03-11 10:10:32 +0800

    Polish framework section, hero layout, and project page styling.

--- static/js/index.js
+++ static/js/index.js
@@ -3,4 +3,31 @@ $(document).ready(function() {
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
   });
+
+  const featureSlides = $(".feature-slide");
+  const featureIndicators = $(".feature-indicator");
+  let featureIndex = 0;
+
+  function setFeature(index) {
+    if (!featureSlides.length) {
+      return;
+    }
+    featureIndex = (index + featureSlides.length) % featureSlides.length;
+    featureSlides.removeClass("is-active").eq(featureIndex).addClass("is-active");
+    featureIndicators.removeClass("is-active").eq(featureIndex).addClass("is-active");
+  }
+
+  $(".feature-nav.prev").click(function() {
+    setFeature(featureIndex - 1);
+  });
+
+  $(".feature-nav.next").click(function() {
+    setFeature(featureIndex + 1);
+  });
+
+  featureIndicators.each(function(index) {
+    $(this).click(function() {
+      setFeature(index);
+    });
+  });
 });
